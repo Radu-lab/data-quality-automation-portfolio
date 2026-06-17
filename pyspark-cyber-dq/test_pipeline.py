4df79d3b9cb068195ea5fa5752c37e90dab02e32
Radu-lab <raduiriciuc@proton.me>
2026-06-17 15:07:14 +0300

    Update test_pipeline file

--- pyspark-cyber-dq/test_pipeline.py
+++ pyspark-cyber-dq/test_pipeline.py
@@ -5,10 +5,7 @@ import pytest
 def test_null_values(spark):
     df_cyber =df_init(spark)
     df_null = df_cyber.filter(col("protocol").isNull()).count()
-    df_bytes = df_cyber.groupBy("attack_type", "protocol").agg(avg("duration").alias("average_duration"),
-                                                               sum("src_bytes").alias("source_bytes"),
-                                                               sum("dst_bytes").alias("destination_bytes")
-                                                               )
+    df_bytes = aggregate2(df_cyber)
     df_bytes_null = df_bytes.filter(col("average_duration").isNull()).count()
 
     assert df_null == 0
@@ -17,8 +14,7 @@ def test_null_values(spark):
 
 def test_valid_values(spark):
     df_cyber = df_init(spark)
-    df_packet = df_cyber.groupBy("protocol").agg(sum("failed_logins").alias("total_failed_logins"),
-                                                 (sum("packet_count").alias("total_packets")))
+    df_packet = aggregate(df_cyber)
 
     df_packet_valid = df_packet.filter(~df_packet.protocol.isin(["UDP","TCP"])).count()
 
